616ab02ed81fc29fa4d10182c235e75576872adf
Soringaming <thehawksquad50@gmail.com>
2016-06-17 10:54:41 +1000

    Fixed erros

--- src/me/soringaming/moon/korra/firecomet/FireComet.java
+++ src/me/soringaming/moon/korra/firecomet/FireComet.java
@@ -10,14 +10,7 @@ import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionDefault;
 import org.bukkit.util.Vector;
 
-<<<<<<< HEAD
-
 import com.projectkorra.projectkorra.BendingPlayer;
-
-
-=======
-import com.projectkorra.projectkorra.BendingPlayer;
->>>>>>> origin/master
 import com.projectkorra.projectkorra.GeneralMethods;
 import com.projectkorra.projectkorra.ProjectKorra;
 import com.projectkorra.projectkorra.ability.Ability;
@@ -25,70 +18,6 @@ import com.projectkorra.projectkorra.ability.AddonAbility;
 import com.projectkorra.projectkorra.ability.FireAbility;
 import com.projectkorra.projectkorra.util.DamageHandler;
 import com.projectkorra.projectkorra.util.ParticleEffect;
-<<<<<<< HEAD
- 
-public class FireComet extends FireAbility implements AddonAbility{
-   
-    private Player player;
-    private double r;
-    private double t;
-    private Location CurrentPLoc;
-    private Location start;
-    private Location loc;
-    private Vector dir;
-    private Permission perm;
-
-    BendingPlayer bp = BendingPlayer.getBendingPlayer(player.getName());
-    private  boolean Charged;
-    private long chargeTime = 1500;
-	private long startTime;
-
-	private double particleHeight;
-	private boolean lowered;
-
-   
-    public FireComet(Player player) {
-        super(player);
-        this.player = player;
-        this.loc = player.getLocation();
-        this.dir = player.getEyeLocation().getDirection().normalize().multiply(1);
-        this.start = player.getLocation();
-        this.lowered = true;
-        this.t = 0;
-        this.r = 1.5;
-       
-    }
- 
-    @Override
-    public long getCooldown() {
-        return 0;
-    }
- 
-    @Override
-    public Location getLocation() {
-        return loc;
-    }
- 
-    @Override
-    public String getName() {
-        return "FireComet";
-    }
- 
-    @Override
-    public boolean isHarmlessAbility() {
-        return false;
-    }
- 
-    @Override
-    public boolean isSneakAbility() {
-        return true;
-    }
- 
-    @Override
-    public void progress() {
-    	this.dir = player.getLocation().getDirection().normalize().multiply(1.5);
-		if(player.isDead() || !player.isOnline()) {
-=======
 
 public class FireComet extends FireAbility implements AddonAbility {
 
@@ -152,7 +81,6 @@ public class FireComet extends FireAbility implements AddonAbility {
 	public void progress() {
 		this.dir = player.getLocation().getDirection().normalize().multiply(1.5);
 		if (player.isDead() || !player.isOnline()) {
->>>>>>> origin/master
 			this.start = player.getEyeLocation();
 			remove();
 			return;
@@ -169,15 +97,10 @@ public class FireComet extends FireAbility implements AddonAbility {
 				Charged = true;
 			}
 			if (player.isSneaking() && startTime + chargeTime > System.currentTimeMillis()) {
-<<<<<<< HEAD
 				doChargeParticles();
-				
-=======
 				start = player.getLocation();
 				loc = player.getEyeLocation();
 				doChargeParticles();
-
->>>>>>> origin/master
 			}
 			if (player.isSneaking() && startTime + chargeTime < System.currentTimeMillis()) {
 				doPlayerChargedParticles();
