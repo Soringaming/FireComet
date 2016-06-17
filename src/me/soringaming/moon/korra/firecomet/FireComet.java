ae44d2e9d8ddcfa53b5f8830c59b5801bd61694a
Soringaming <thehawksquad50@gmail.com>
2016-06-17 10:49:12 +1000

    Bug fixes..... every fuckin where x'D

--- src/me/soringaming/moon/korra/firecomet/FireComet.java
+++ src/me/soringaming/moon/korra/firecomet/FireComet.java
@@ -1,5 +1,5 @@
 package me.soringaming.moon.korra.firecomet;
- 
+
 import java.util.logging.Level;
 
 import org.bukkit.Location;
@@ -18,69 +18,73 @@ import com.projectkorra.projectkorra.ability.AddonAbility;
 import com.projectkorra.projectkorra.ability.FireAbility;
 import com.projectkorra.projectkorra.util.DamageHandler;
 import com.projectkorra.projectkorra.util.ParticleEffect;
- 
-public class FireComet extends FireAbility implements AddonAbility{
-   
-    private Player player;
-    private double r;
-    private double t;
-    private Location CurrentPLoc;
-    private Location start;
-    private Location loc;
-    private Vector dir;
-    private Permission perm;
-    BendingPlayer bp = BendingPlayer.getBendingPlayer(player.getName());
-    private  boolean Charged;
-    private long chargeTime = 1500;
+
+public class FireComet extends FireAbility implements AddonAbility {
+
+	private Player player;
+	private double r;
+	private double t;
+	private Location CurrentPLoc;
+	private Location start;
+	private Location loc;
+	private Vector dir;
+	private Permission perm;
+	BendingPlayer bp;
+	private boolean Charged;
+	private long chargeTime;
 	private long startTime;
 	private double particleHeight;
 	private boolean lowered;
-   
-    public FireComet(Player player) {
-        super(player);
-        this.player = player;
-        this.loc = player.getLocation();
-        this.dir = player.getEyeLocation().getDirection().normalize().multiply(1);
-        this.start = player.getLocation();
-        this.lowered = true;
-        this.t = 0;
-        this.r = 1.5;
-       
-    }
- 
-    @Override
-    public long getCooldown() {
-        return 0;
-    }
- 
-    @Override
-    public Location getLocation() {
-        return loc;
-    }
- 
-    @Override
-    public String getName() {
-        return "FireComet";
-    }
- 
-    @Override
-    public boolean isHarmlessAbility() {
-        return false;
-    }
- 
-    @Override
-    public boolean isSneakAbility() {
-        return true;
-    }
- 
-    @Override
-    public void progress() {
-    	this.dir = player.getLocation().getDirection().normalize().multiply(1.5);
-		if(player.isDead() || !player.isOnline()) {
+
+	public FireComet(Player player) {
+		super(player);
+		this.player = player;
+		bp = BendingPlayer.getBendingPlayer(player.getName());
+		this.loc = player.getLocation();
+		this.dir = player.getEyeLocation().getDirection().normalize().multiply(1);
+		this.start = player.getLocation();
+		this.lowered = true;
+		this.startTime = System.currentTimeMillis();
+		this.chargeTime = 15000;
+		this.t = 0;
+		this.r = 1.5;
+		start();
+
+	}
+
+	@Override
+	public long getCooldown() {
+		return 0;
+	}
+
+	@Override
+	public Location getLocation() {
+		return loc;
+	}
+
+	@Override
+	public String getName() {
+		return "FireComet";
+	}
+
+	@Override
+	public boolean isHarmlessAbility() {
+		return false;
+	}
+
+	@Override
+	public boolean isSneakAbility() {
+		return true;
+	}
+
+	@Override
+	public void progress() {
+		this.dir = player.getLocation().getDirection().normalize().multiply(1.5);
+		if (player.isDead() || !player.isOnline()) {
 			this.start = player.getEyeLocation();
 			remove();
 			return;
-				
+
 		}
 		if (!Charged) {
 			if (!player.isSneaking() && startTime + chargeTime > System.currentTimeMillis()) {
@@ -96,88 +100,91 @@ public class FireComet extends FireAbility implements AddonAbility{
 				start = player.getLocation();
 				loc = player.getEyeLocation();
 				doChargeParticles();
-				
+
 			}
 			if (player.isSneaking() && startTime + chargeTime < System.currentTimeMillis()) {
 				doPlayerChargedParticles();
 				doBallChargedParticles();
-				
+
 			}
-		}else {
-			loc.add(dir);
-			doBallChargedParticles();
+		} else {
 			bp.addCooldown((Ability) this);
-		}
-		
-		if (GeneralMethods.isSolid(loc.getBlock())) {
-			remove();
-			return;
-		}
-		if (isWater(loc.getBlock())) {
-			remove();
-			return;
-		}
-		if (loc.distance(start) > 40) {
-			remove();
-			return;
-		}
-		
-		for (Entity e : GeneralMethods.getEntitiesAroundPoint(loc, 3)) {
-			if (e instanceof LivingEntity && e.getEntityId() != player.getEntityId()) {
-				DamageHandler.damageEntity(e, 8, this);
+			loc.add(dir);
+			doBallThrowParticles();
+
+			if (GeneralMethods.isSolid(loc.getBlock())) {
+				remove();
+				return;
 			}
-			if(player.isSneaking()) {
-				this.player.getEyeLocation();
-				this.dir = player.getLocation().getDirection().normalize().multiply(1.1);
-				this.start = player.getEyeLocation();
-				
+			if (isWater(loc.getBlock())) {
+				remove();
+				return;
 			}
-			
+			if (loc.distance(start) > 40) {
+				remove();
+				return;
+			}
+
+			for (Entity e : GeneralMethods.getEntitiesAroundPoint(loc, 3)) {
+				if (e instanceof LivingEntity && e.getEntityId() != player.getEntityId()) {
+					DamageHandler.damageEntity(e, 8, this);
+				}
+				if (player.isSneaking()) {
+					this.player.getEyeLocation();
+					this.dir = player.getLocation().getDirection().normalize().multiply(1.1);
+					this.start = player.getEyeLocation();
+				}
+
+			}
+
 		}
-		
-    }
-   
-    @Override
-    public String getDescription() {
-        return getVersion() + " Developed By " + getAuthor() + ":\nA Test Ability";
-       
-    }
-   
-    public void doChargeParticles() {
-        t = t + Math.PI / 32;
-        if(r <= 5) {
-        	r += 0.5;
-        } else {
-        	r = 0.5;
-        }
-        double x = r * Math.sin(t);
-        double y = 0;
-        double z = r * Math.cos(t);
-        CurrentPLoc.add(x, y, z);
-        ParticleEffect.FLAME.display(CurrentPLoc, 0.1F, 0.1F, 0.1F, 0.05F, 2);
-        CurrentPLoc.subtract(x, y, z);
-       
-    }
-    
-    public void doPlayerChargedParticles() {
-    	if(r != 5) {
-    		r = 5;
-    	}
-    	t = t + Math.PI / 8;
-    	double x = r * Math.sin(t);
-    	double y = t;
-    	if(y >= 6) {
-    		y = 0;
-    	}
-    	double z = r * Math.cos(t);
-    	CurrentPLoc.add(x, y, z);
-    	ParticleEffect.FLAME.display(CurrentPLoc, 0.5F, 0.5F, 0.5F, 0.5F, 5);
-    	CurrentPLoc.subtract(x, y, z);
-    }
-    
-    public void doBallChargedParticles() {
+
+	}
+
+	@Override
+	public String getDescription() {
+		return getVersion() + " Developed By " + getAuthor() + ":\nA Test Ability";
+
+	}
+
+	public void doChargeParticles() {
+		t = t + Math.PI / 32;
+		CurrentPLoc = player.getLocation();
+		if (r <= 5) {
+			r += 0.5;
+		} else {
+			r = 0.5;
+		}
+		double x = r * Math.sin(t);
+		double y = 0;
+		double z = r * Math.cos(t);
+		CurrentPLoc.add(x, y, z);
+		ParticleEffect.FLAME.display(CurrentPLoc, 0.1F, 0.1F, 0.1F, 0F, 2);
+		CurrentPLoc.subtract(x, y, z);
+
+	}
+
+	public void doPlayerChargedParticles() {
+		if (r != 5) {
+			r = 5;
+		}
+		t = t + Math.PI / 8;
+		CurrentPLoc = player.getLocation();
+		double x = r * Math.sin(t);
+		double y = t;
+		if (y >= 6) {
+			y = 0;
+		}
+		double z = r * Math.cos(t);
+		CurrentPLoc.add(x, y, z);
+		ParticleEffect.FLAME.display(CurrentPLoc, 0.5F, 0.5F, 0.5F, 0F, 5);
+		CurrentPLoc.subtract(x, y, z);
+	}
+
+	public void doBallChargedParticles() {
 		Location Currentloc = GeneralMethods.getTargetedLocation(player, 5);
 		t = t + Math.PI / 8;
+		double r2 = 2;
 		if (particleHeight <= 4 && lowered == true) {
 			particleHeight += 0.5;
 			if (particleHeight == 4) {
@@ -189,45 +196,76 @@ public class FireComet extends FireAbility implements AddonAbility{
 				lowered = true;
 			}
 		}
-		double x = r * Math.cos(t);
+		double x = r2 * Math.cos(t);
 		double y = particleHeight;
-		double z = r * Math.sin(t);
+		double z = r2 * Math.sin(t);
 		Currentloc.add(x, y, z);
-		ParticleEffect.FLAME.display(Currentloc, 0.1F, 0.1F, 0.1F, 0.05F, 3);
+		ParticleEffect.FLAME.display(Currentloc, 0.1F, 0.1F, 0.1F, 0.005F, 50);
 		Currentloc.subtract(x, y, z);
 
-		double x2 = r * Math.sin(t);
+		double x2 = r2 * Math.sin(t);
 		double y2 = particleHeight;
-		double z2 = r * Math.cos(t);
+		double z2 = r2 * Math.cos(t);
 		Currentloc.add(x2, y2, z2);
-		ParticleEffect.FLAME.display(Currentloc, 0.1F, 0.1F, 0.1F, 0.05F, 3);
+		ParticleEffect.FLAME.display(Currentloc, 0.1F, 0.1F, 0.1F, 0.005F, 50);
 		Currentloc.subtract(x2, y2, z2);
-    }
- 
-    @Override
-    public String getAuthor() {
-        return "Soringaming & Moon243"; //Moon was here :P
-    }
- 
-    @Override
-    public String getVersion() {
-        return "v1.0";
-    }
- 
-    @Override
-    public void load() {
-        ProjectKorra.plugin.getServer().getLogger().log(Level.INFO, getName() + " " + getVersion() + " Developed By: " + getAuthor() + " Has Been Enabled");
-        ProjectKorra.plugin.getServer().getPluginManager().registerEvents(new FireCometListener(), ProjectKorra.plugin);   
-        perm = new Permission("bending.ability.FireComet");
-        perm.setDefault(PermissionDefault.TRUE);
-        ProjectKorra.plugin.getServer().getPluginManager().addPermission(perm);
-    }
- 
-    @Override
-    public void stop() {
-        ProjectKorra.plugin.getServer().getLogger().log(Level.INFO, getName() + " " + getVersion() + " Developed By: " + getAuthor() + " Has Been Disabled");
-        ProjectKorra.plugin.getServer().getPluginManager().removePermission(perm);
-        super.remove();
-    }
- 
+	}
+	
+	public void doBallThrowParticles() {
+		t = t + Math.PI / 8;
+		double r2 = 2;
+		if (particleHeight <= 4 && lowered == true) {
+			particleHeight += 0.5;
+			if (particleHeight == 4) {
+				lowered = false;
+			}
+		} else {
+			particleHeight -= 0.5;
+			if (particleHeight <= 0.5) {
+				lowered = true;
+			}
+		}
+		double x = r2 * Math.cos(t);
+		double y = particleHeight;
+		double z = r2 * Math.sin(t);
+		loc.add(x, y, z);
+		ParticleEffect.FLAME.display(loc, 0.1F, 0.1F, 0.1F, 0.005F, 50);
+		loc.subtract(x, y, z);
+
+		double x2 = r2 * Math.sin(t);
+		double y2 = particleHeight;
+		double z2 = r2 * Math.cos(t);
+		loc.add(x2, y2, z2);
+		ParticleEffect.FLAME.display(loc, 0.1F, 0.1F, 0.1F, 0.005F, 50);
+		loc.subtract(x2, y2, z2);
+	}
+
+	@Override
+	public String getAuthor() {
+		return "Soringaming & Moon243"; // Moon was here :P
+	}
+
+	@Override
+	public String getVersion() {
+		return "v1.0";
+	}
+
+	@Override
+	public void load() {
+		ProjectKorra.plugin.getServer().getLogger().log(Level.INFO,
+				getName() + " " + getVersion() + " Developed By: " + getAuthor() + " Has Been Enabled");
+		ProjectKorra.plugin.getServer().getPluginManager().registerEvents(new FireCometListener(), ProjectKorra.plugin);
+		perm = new Permission("bending.ability.FireComet");
+		perm.setDefault(PermissionDefault.TRUE);
+		ProjectKorra.plugin.getServer().getPluginManager().addPermission(perm);
+	}
+
+	@Override
+	public void stop() {
+		ProjectKorra.plugin.getServer().getLogger().log(Level.INFO,
+				getName() + " " + getVersion() + " Developed By: " + getAuthor() + " Has Been Disabled");
+		ProjectKorra.plugin.getServer().getPluginManager().removePermission(perm);
+		super.remove();
+	}
+
 }
