2f179e65f1d1cb21dee6e5425448f6b1172f8e99
Moon243 <Gideonssolock@gmail.com>
2016-06-16 17:11:04 -0700

    Made the progress method

--- src/me/soringaming/moon/korra/firecomet/FireComet.java
+++ src/me/soringaming/moon/korra/firecomet/FireComet.java
@@ -3,14 +3,20 @@ package me.soringaming.moon.korra.firecomet;
 import java.util.logging.Level;
  
 import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionDefault;
 import org.bukkit.util.Vector;
- 
+
+import com.projectkorra.projectkorra.BendingPlayer;
+import com.projectkorra.projectkorra.GeneralMethods;
 import com.projectkorra.projectkorra.ProjectKorra;
+import com.projectkorra.projectkorra.ability.Ability;
 import com.projectkorra.projectkorra.ability.AddonAbility;
 import com.projectkorra.projectkorra.ability.FireAbility;
+import com.projectkorra.projectkorra.util.DamageHandler;
 import com.projectkorra.projectkorra.util.ParticleEffect;
  
 public class FireComet extends FireAbility implements AddonAbility{
@@ -22,6 +28,10 @@ public class FireComet extends FireAbility implements AddonAbility{
     private Location loc;
     private Vector dir;
     private Permission perm;
+    BendingPlayer bp = BendingPlayer.getBendingPlayer(player.getName());
+    private  boolean Charged;
+    private long chargeTime = 1500;
+	private long startTime;
    
     public FireComet(Player player) {
         super(player);
@@ -61,7 +71,59 @@ public class FireComet extends FireAbility implements AddonAbility{
  
     @Override
     public void progress() {
-       
+    	this.dir = player.getLocation().getDirection().normalize().multiply(1.5);
+		if(player.isDead() || !player.isOnline()) {
+			this.start = player.getEyeLocation();
+			remove();
+			return;
+				
+		}
+		if (!Charged) {
+			if (!player.isSneaking() && startTime + chargeTime > System.currentTimeMillis()) {
+				remove();
+				return;
+			}
+			if (!player.isSneaking() && startTime + chargeTime < System.currentTimeMillis()) {
+				Charged = true;
+			}
+			if (player.isSneaking() && startTime + chargeTime > System.currentTimeMillis()) {
+				doChargeParicles();
+				
+			}
+			if (player.isSneaking() && startTime + chargeTime < System.currentTimeMillis()) {
+				player.setSneaking(false);
+			}
+		}else {
+			loc.add(dir);
+			bp.addCooldown((Ability) this);
+		}
+		
+		if (GeneralMethods.isSolid(loc.getBlock())) {
+			remove();
+			return;
+		}
+		if (isWater(loc.getBlock())) {
+			remove();
+			return;
+		}
+		if (loc.distance(start) > 40) {
+			remove();
+			return;
+		}
+		
+		for (Entity e : GeneralMethods.getEntitiesAroundPoint(loc, 3)) {
+			if (e instanceof LivingEntity && e.getEntityId() != player.getEntityId()) {
+				DamageHandler.damageEntity(e, 8, this);
+			}
+			if(player.isSneaking()) {
+				this.player.getEyeLocation();
+				this.dir = player.getLocation().getDirection().normalize().multiply(1.1);
+				this.start = player.getEyeLocation();
+				
+			}
+			
+		}
+		
     }
    
     @Override
